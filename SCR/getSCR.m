e82c2afca60c52688e28ff81a8b9d6935f87971f
scholp94 <scholp@uwalumni.com>
2021-05-11 10:54:55 -0500

    Updated binder

--- SCR/getSCR.m
+++ SCR/getSCR.m
@@ -1,4 +1,4 @@
-function [LinExponent] = getSCR(sdata, Fs, name, frmsize)
+function [LinExponent] = getSCR(sdata, Fs, ~, frmsize)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Calculates convergence ratio (labelled as LinExponent) of a given sample
 % Note: this script assumes samples have already been trimmed
@@ -12,7 +12,7 @@ wsize = floor(frmsize*Fs);
 
 % The Short-Time-Fourier-Transform with phase
 
-[S, Sphase, xlen ] = time_freq_analysis(sdata, wsize); 
+[S, ~, ~ ] = time_freq_analysis(sdata, wsize); 
 % Produces equally sized spectrograms of the voice samples.
 % S is the spectrogram, see function below
 
@@ -20,10 +20,11 @@ wsize = floor(frmsize*Fs);
 S = abs(S);
 maxPower = max(S(:));
 S = S / maxPower;
+S = S'; %transpose to remove warnings
 
 %Calculating Convergence Ratio: -log(LinRatio)  ~labeled as LinExponent~
-SMax = max(S');
+SMax = max(S);
 % transpose used because we are graphing up the frequency axis of the 
 % spectrogram, not down the time axis
-SMin = min(S');
+SMin = min(S);
 LinExponent = -log((sum(SMax-SMin)/sum(SMax)));
