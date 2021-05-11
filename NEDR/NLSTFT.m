7c8326efe3e89b874d1ef53f43adc704e1d448d4
scholp94 <scholp@uwalumni.com>
2021-05-11 09:52:54 -0500

    altered file path

---
+++ NEDR/NLSTFT.m
@@ -0,0 +1,70 @@
+function tfr = NLSTFT(x,c,fs,hlength);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%      Non-linear Short time Fourier transform.
+%	x      : Signal.
+%	c      : First order derivative of signal IF.
+%	fs     : Sample Frequency .
+%	hlength: Length of window function.
+
+%	tfr    : Time-Frequency Representation.
+
+%
+%  This program is free software; you can redistribute it and/or modify
+%  it according to your requirement.
+%
+%  This program is distributed in the hope that it will be useful,
+%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+%
+%   Written by Gang Yu in Shandong University at 2015.4.28.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[xrow,xcol] = size(x);
+if (nargin < 3),
+error('At least 3 parameter is required');
+end;
+Siglength=xrow;
+
+if (nargin < 4),
+hlength=floor(Siglength/4);
+end;
+
+hlength=hlength+1-rem(hlength,2);
+h = tftb_window(hlength);
+
+%t=1:xrow;
+%[trow,tcol] = size(t);
+
+[hrow,hcol]=size(h); Lh=(hrow-1)/2; 
+
+h=h/norm(h);
+
+
+if (xcol~=1),
+ error('X must have one column');
+end; 
+
+ N=xrow;
+ t=1:xrow;
+
+[trow,tcol] = size(t);
+ 
+
+tt=(1:N)/fs;
+tfr= zeros (N,tcol) ; 
+
+
+for icol=1:tcol,
+ ti= t(icol); tau=-min([round(N/2)-1,Lh,ti-1]):min([round(N/2)-1,Lh,xrow-ti]);
+ indices= rem(N+tau,N)+1; 
+
+
+ rSig = x(ti+tau,1);
+ %rSig = Hilbert(real(rSig));
+
+a=Lh+1+tau;
+
+tfr(indices,icol)=rSig.*conj(h(Lh+1+tau)).*exp(j * 2.0 * pi * (c(icol)/2) * (tt(ti+tau)-tt(icol)).^2)';
+
+end;
+tfr=fft(tfr); 
---
+++ SCR/batchSCR.m
@@ -0,0 +1,82 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% batchSCR.m
+%
+% Use this script to calculate SCR values on cleaned .wav files that have
+% been trimmed to be 0.75 seconds long.
+%
+% Created by: Andrew Vamos - 9/15/16
+% Original SCR script by Dr. Liyu Lin - 2015
+%
+% Output files will be saved in this directory's "Results" folder.
+%
+% Window size for STFT = 0.012 seconds
+% 0.75 second voice segments were used in paper also here in this script
+%
+% Last edited by: Austin J. Scholp, MS on 5/7/2021
+%
+% Edited for usability and removed unnecessary scripts
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clc; clear; close
+
+%% Load Files
+%Prompts the user to choose a .wav files for anlysis
+disp('Select .wav files for analysis. You MUST choose more than one.');
+[filenames, path] = uigetfile('*.wav', 'MultiSelect', 'on');
+
+if isa(filenames, 'double')==1 %check if filenames is a number
+    if filenames==0 %if it is, check if it equals zero
+        disp('File selection cancelled.');%If it does equal zero
+        return
+    end
+end
+if isa(filenames, 'char')==1 %check if filenames is single string
+    disp('Please select more than one file.'); %If it is
+    return
+end
+
+wavFiles = cell(1,length(filenames));
+data = cell(1,length(filenames));
+Fs = zeros(1,length(filenames));
+
+for k = 1:length(filenames)
+    wavFiles{k} = dir(strcat([path filenames{k}]));
+end
+
+%get data of selected files
+for k = 1:length(filenames)
+    [data{k}, Fs(k)]= audioread(strcat([path filenames{k}]));
+end
+
+disp('Please wait.');
+%% Calculate SCR
+
+scrResults = {'Filename' 'SCR Value'}; % preallocate, lazy/easy/quick
+windowLength = 0.012; % Don't change this unless you know what you're doing
+
+%run SCR script for each wav file
+for  h = 1:k
+    data_temp=data{h};
+    data_cc=data_temp(1000:fix(1000+Fs(h)*0.7-1));
+    scrVal = getSCR(data_cc, Fs(h), filenames{h}, windowLength);
+    scrResults{h+1,1} = filenames{h} ;
+    scrResults{h+1,2} = strcat([' ' num2str(scrVal)]);
+end
+
+clearvars -except scrResults %deletes unused/temp variables
+%% Format and write output
+%Convert data to table for easy printing
+dataTable = cell2table(scrResults);
+
+fileTime = datestr(datetime);
+fileTime = strrep(fileTime, ':', '-');
+fileTime = strrep(fileTime, ' ', '_');
+
+%Make a directory for the results if one does not exist
+[~,~] = mkdir('Results');
+
+%Print table to results directory
+writetable(dataTable, strcat(['Results\SCR_Results_' fileTime]));
+disp('SCR results listed in file: ');
+disp(strcat(['SCR_Results_' fileTime]));
+clear status fileTime msg 
